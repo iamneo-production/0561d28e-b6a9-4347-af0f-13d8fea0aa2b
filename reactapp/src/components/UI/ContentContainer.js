7e800e74480395d0813683f0f04a96cb140283a6
loges21 <70424210+loges21@users.noreply.github.com>
2022-03-28 11:59:39 +0530

    Update ContentContainer.js

--- reactapp/src/components/UI/ContentContainer.js
+++ reactapp/src/components/UI/ContentContainer.js
@@ -5,6 +5,7 @@ function ContentContainer(props) {
   return <Container fluid style={{height:"85vh ",overflowX:"hidden"}}><Row className="justify-content-center">
       <Col sm={8} md={10} className=" mb-2 rounded">{props.children}</Col>
   </Row></Container>;
+  //changes
 }
 
 export default ContentContainer;
