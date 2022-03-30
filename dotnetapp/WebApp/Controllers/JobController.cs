f37501123f9de46c1867298383951be5b90b527a
Uday Kiran Bhashamalla <73292774+UdayKiranBhashamalla@users.noreply.github.com>
2022-03-30 12:25:54 +0530

    Create JobController.cs

---
+++ dotnetapp/WebApp/Controllers/JobController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatabaseController;
+using CookHiring.Models;
+
+namespace CookHiring.Controllers
+{
+    public class JobController : ControllerBase
+    {
+        private BusinessLayer bsl = new BusinessLayer();
+        [Route("admin/addJob/{id}")]
+        [HttpPost]
+        public string addJob([FromBody]JobModel job, [FromHeader]string id)
+        {
+            return bsl.addJob(job, id);
+        }
+        [Route("admin/editJob/{id}")]
+        [HttpPut]
+        public string editJob([FromHeader] string id, [FromBody]JobModel job)
+        {
+            return bsl.editJob(id, job);
+
+        }
+        [Route("user/dashboard")]
+        [HttpGet]
+        public object getJobs()
+        {
+            var jobs =  bsl.getJobs();
+            return jobs;
+        }
+        //JobProviderDashboard
+        [Route("customer/dashboard")]
+        [HttpGet]
+        public object getCustomerJobs(string id)
+        {
+            var jobs = bsl.getcustomerJobs(id);
+            return jobs;
+        }
+        [Route("admin/deleteJob/{jobId}")]
+        [HttpPost]
+        public string deleteJob(String jobId)
+        {
+            return bsl.deleteJob(jobId);
+        }
+        [Route("admin/getCandidates")]
+        [HttpGet]
+        public object candidates()
+        {
+            var res = bsl.candidates();
+            return res;
+        }
+    }
+}
