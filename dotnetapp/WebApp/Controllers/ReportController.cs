b76dd4ee3ffbcc19468e82bf24a36d12441a9e2a
mrconqu3ror <73517199+mrconqu3ror@users.noreply.github.com>
2022-03-31 16:16:36 +0530

    Create ReportController.cs

---
+++ dotnetapp/WebApp/Controllers/ReportController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using DatabaseController;
+
+namespace CookHiring.Controllers
+{
+    public class ReportController : Controller
+    {
+        private BusinessLayer bsl = new BusinessLayer();
+        
+        [Route("admin/report/totalUsers")]
+        [HttpGet]
+        public int totalUsers()
+        {
+            return bsl.totalUsers();
+        }
+        [Route("admin/report/totalJobSeekers")]
+        [HttpGet]
+        public int totalJobSeekers()
+        {
+            return bsl.totalJobSeekers();
+        }
+        [Route("admin/report/totalJobProviders")]
+        [HttpGet]
+        public int totalJobProviders()
+        {
+            return bsl.totalJobProviders();
+        }
+        [Route("admin/report/search/{location}")]
+        [HttpGet]
+        public int totalJobsByLocation(string location)
+        {
+            return bsl.totalJobsByLocation(location);
+        }
+
+    }
+}
